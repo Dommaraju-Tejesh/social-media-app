3d50eae41e97e9c4b67a5f4111bb8ff718267d89
Tejesh <d.tejeshraju67@gmail.com>
2026-01-07 21:46:47 +0530

    Fix mobile navbar toggle by adding Bootstrap JS

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import "./index.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; // ✅ ADD THIS LINE
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -15,3 +16,4 @@ root.render(
     </AuthProvider>
   </BrowserRouter>
 );
+
